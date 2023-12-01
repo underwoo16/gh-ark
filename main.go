8fce7e9af62491e56b6ac36eea69b6cae1587f4c
Chandler Underwood <underwoo16@up.edu>
2023-12-01 15:14:23 -0800

    added logging

--- main.go
+++ main.go
@@ -29,26 +29,34 @@ func main() {
 	err = gitService.CherryPick(latestCommit)
 	if err != nil {
 		// TODO: abort chery-pick and switch back to main
+		fmt.Println("Cherry-pick failed, need to --quit...")
 		log.Fatal(err)
 	}
 
+	fmt.Println("Cherry-pick succeeded, pushing to remote...")
 	err = gitService.Push()
 	if err != nil {
+		fmt.Println("Push failed...")
 		log.Fatal(err)
 	}
 
+	fmt.Println("Push succeeded, creating PR...")
 	// create pr
 	ghService := gh.NewGitHubService()
 	err = ghService.CreatePullRequest(branchName, "main")
 	if err != nil {
+		fmt.Println("PR creation failed...")
 		log.Fatal(err)
 	}
 
+	fmt.Println("PR created, switching back to main...")
 	//switch back to main
 	err = gitService.Switch("main")
 	if err != nil {
+		fmt.Println("Switching back to main failed...")
 		log.Fatal(err)
 	}
+	fmt.Println("DONE!")
 }
 
 // For more examples of using go-gh, see:
